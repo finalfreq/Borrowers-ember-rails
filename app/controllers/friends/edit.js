7458b8e99204acc9e70225df32e62f0fea72eb12
James Williams <jamesrwilliams91@gmail.com>
2015-07-21 23:50:19 -0700

    add edit page for friends

---
+++ app/controllers/friends/edit.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+var computed = Ember.computed
+export default Ember.Controller.extend({
+  isValid: computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    function() {
+      return !Ember.isEmpty(this.get('model.email')) &&
+        !Ember.isEmpty(this.get('model.firstName')) &&
+        !Ember.isEmpty(this.get('model.lastName')) &&
+        !Ember.isEmpty(this.get('model.twitter'));
+      }
+  ),
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        var _this = this;
+        this.get('model').save().then(function(friend){
+          _this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'you have to fill out all forms');
+      }
+      return false
+    },
+    cancel: function() {
+      this.transitionToRoute('friends.show', this.get('model'));
+      return false
+    }
+  }
+});
