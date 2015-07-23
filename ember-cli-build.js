5452c5301486c6617ab12128ffb5c04b5aea9cab
James Williams <jamesrwilliams91@gmail.com>
2015-07-22 23:51:18 -0700

    add delete function to friends, install picnicss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('vendor/fontello/fontello.css');
+  app.import('vendor/fontello/font/fontello.ttf',{￼￼￼￼￼￼￼￼￼￼￼￼ ￼￼￼￼￼destDir: 'font' });
+  app.import('vendor/fontello/font/fontello.eot',{ destDir: 'font' });
+  app.import('vendor/fontello/font/fontello.svg',{  destDir: 'font' });
+  app.import('vendor/fontello/font/fontello.woff',{ destDir: 'font' });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
