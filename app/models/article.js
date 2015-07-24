7e6fab3ae8a1feeb8b7fd974bdf5f8ae457c6812
James Williams <jamesrwilliams91@gmail.com>
2015-07-23 23:49:07 -0700

    add article class to ember

---
+++ app/models/article.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+var attr = DS.attr
+export default DS.Model.extend({
+  createdAt: attr('date'),
+  description: attr('string'),
+  notes: attr('string'),
+  state: attr('string'),
+  friend: DS.belongsTo('friend')
+});
